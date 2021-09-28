c34055e6f6df71069b3deebe0bd89729f9f534f9
David Josué Marcial Quero <A00828702@itesm.mx>
2021-09-27 19:37:06 -0700

    Initial Commit

---
+++ App03-Lista de Colores/App03_Lista_de_ColoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  App03_Lista_de_ColoresApp.swift
+//  App03-Lista de Colores
+//
+//  Created by David Josué Marcial Quero on 27/09/21.
+//
+
+import SwiftUI
+
+@main
+struct App03_Lista_de_ColoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App03-Lista de Colores/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App03-Lista de Colores
+//
+//  Created by David Josué Marcial Quero on 27/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
